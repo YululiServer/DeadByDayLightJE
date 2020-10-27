11cf15b3038f4e77f239a2233788d1cb7aa5b497
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-10-27 23:03:20 +0900

    コンフィグの更新

--- src/main/kotlin/com/github/rain1208/deadbydaylightje/maps/Map.kt
+++ src/main/kotlin/com/github/rain1208/deadbydaylightje/maps/Map.kt
@@ -5,25 +5,25 @@ import org.bukkit.Bukkit
 import org.bukkit.Location
 
 class Map {
-    val spawnPoint = arrayListOf<Location>()
-    val killerSpawn = arrayListOf<Location>()
+    private val spawnPoint = arrayListOf<Location>()
+    private val killerSpawn = arrayListOf<Location>()
     val generatorPoint = arrayListOf<Location>()
 
     init {
         val config = DeadByDayLightJE.instance.config
         val world = Bukkit.getWorld(config.getString("world.name") ?: "world") ?: Bukkit.getWorld("world")
 
-        val spawnList = config.getStringList("world.spawn")
-        for (spawn in spawnList) {
+        for (spawn in config.getStringList("world.spawn")) {
             val pos = spawn.split(",").map { it.toDouble() }
             spawnPoint.add(Location(world,pos[0],pos[1],pos[2],0f,0f))
         }
-
-        val pos = config.getString("world.killerSpawn")?.split(",")?.map { it.toDouble() } ?: "0,10,0".split(",").map { it.toDouble() }
-        killerSpawn.add(Location(world,pos[0],pos[1],pos[2],0f,0f))
+        for (spawn in config.getStringList("world.killerSpawn")) {
+            val pos = spawn.split(",").map { it.toDouble() }
+            killerSpawn.add(Location(world,pos[0],pos[1],pos[2],0f,0f))
+        }
     }
 
     fun getSpawn():Location = spawnPoint.random()
 
-    fun getKillerSpawn(): Location = killerSpawn[0]
+    fun getKillerSpawn(): Location = killerSpawn.random()
 }
