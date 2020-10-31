18836283cd068ea05bea80ac39d60f00acef4994
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-11-01 00:27:57 +0900

    発電機の破壊

--- src/main/kotlin/com/github/rain1208/deadbydaylightje/maps/Generator.kt
+++ src/main/kotlin/com/github/rain1208/deadbydaylightje/maps/Generator.kt
@@ -3,6 +3,7 @@ package com.github.rain1208.deadbydaylightje.maps
 import com.github.rain1208.deadbydaylightje.game.Game
 import org.bukkit.Bukkit
 import org.bukkit.Particle
+import org.bukkit.Sound
 import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.Player
 
@@ -20,13 +21,20 @@ class Generator(val armorStand: ArmorStand) {
         sendParticle()
         val nearPlayer = armorStand.getNearbyEntities(1.5,2.0,1.5).filterIsInstance<Player>()
         for (entity in nearPlayer) {
-            if (entity.isSneaking) game.getPlayer(entity)?.onUse(this)
+            if (entity.isSneaking) {
+                game.getPlayer(entity)?.onUse(this)
+            } else {
+                val n = (occupancyRate / 10).toInt()
+                val msg = StringBuilder("修理率 :"+"■".repeat(n)+"□".repeat(10 - n)).toString()
+                entity.sendTitle("",msg,0,25,0)
+            }
         }
 
         if (occupancyRate >= 100) {
             for (player in nearPlayer) {
                 if (game.getSurvivors().contains(player)) {
                     game.survivor[player.name]?.repairComplete()
+                    game.generatorCount--
                 }
             }
             isAlive = false
@@ -42,6 +50,8 @@ class Generator(val armorStand: ArmorStand) {
     }
 
     fun onBreak(breakAbility: Double) {
+        armorStand.world.playSound(armorStand.location, Sound.ENTITY_GENERIC_EXPLODE,2f,1f)
+        armorStand.world.spawnParticle(Particle.EXPLOSION_HUGE,armorStand.location,1)
         if (occupancyRate - breakAbility <= 0) {
             occupancyRate = 0.0
         } else {
