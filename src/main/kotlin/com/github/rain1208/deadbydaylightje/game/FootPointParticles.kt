0141c79290b64e734d36c26255edcd40c4f14287
raink1208 <58714901+raink1208@users.noreply.github.com>
2020-11-14 13:02:37 +0900

    足跡の見える範囲を変更

--- src/main/kotlin/com/github/rain1208/deadbydaylightje/game/FootPointParticles.kt
+++ src/main/kotlin/com/github/rain1208/deadbydaylightje/game/FootPointParticles.kt
@@ -13,7 +13,9 @@ class FootPointParticles(val game: Game): BukkitRunnable() {
         for ((_, locations) in pointList) {
             for (pos in locations) {
                 for (killer in game.getKillers()) {
-                    killer.spawnParticle(Particle.DRIP_LAVA,pos,10,0.3,0.2,0.3)
+                    if (pos.distance(killer.location) <= 30) {
+                        killer.spawnParticle(Particle.DRIP_LAVA,pos,10,0.3,0.2,0.3)
+                    }
                 }
             }
         }
